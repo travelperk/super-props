a188133e9c8cdfd6d81f8c0940e4df9a17c39aad
Gpx <polvara@gmail.com>
2019-02-09 14:16:28 +0100

    feat: 🎸 add email()

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as boolean } from "./boolean";
 export { default as number } from "./number";
 export { default as integer } from "./integer";
 export { default as string } from "./string";
+export { default as email } from "./email";
